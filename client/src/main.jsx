7c548a5eb9e024ea0588875fdb77791fd1e365a6
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-11-04 15:28:34 +0530

    chore(router): set up React Router with a basic route configuration.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+// components...
+import App from "./App.jsx";
+
+const router = createBrowserRouter([
+  { 
+   path: "/",
+   element: <App />,
+   children:[],
+   },
+
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
