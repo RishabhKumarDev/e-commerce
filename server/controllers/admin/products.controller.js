1ce6568f3d486f793ce40d1e46a880c0ce5cd69b
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-11-24 13:18:17 +0530

    feat(server/admin): implement CRUD operations for products with appropriate routes and model.

--- server/controllers/admin/products.controller.js
+++ server/controllers/admin/products.controller.js
@@ -1,5 +1,7 @@
 import { imageUploadUtil } from "../../helpers/cloudinary.js";
+import { Product } from "../../models/product.model.js";
 import { ApiResponse } from '../../utils/ApiResponse.js';
+import { ApiError } from '../../utils/ApiError.js';
 
 const handleProductImageUpload = async (req, res) => {
 
@@ -11,4 +13,54 @@ const handleProductImageUpload = async (req, res) => {
         .json(new ApiResponse(200, { result }, "Image Uploaded Successfully"))
 }
 
-export { handleProductImageUpload }
+// add a new product
+const addProduct = async (req, res) => {
+    const { image, title, description, category, brand, price, salePrice, totalStock } = req.body;
+
+    const addedProduct = new Product({ image, title, description, category, brand, price, salePrice, totalStock })
+
+    await addedProduct.save();
+    res.status(201).json(new ApiResponse(201, addedProduct, "successfully added the product"))
+}
+
+
+// featch all product
+const fetchAllProducts = async (req, res) => {
+    const productList = await Product.find({});
+
+    res.status(200).json(new ApiResponse(200, productList, "list of products featched successfully"))
+}
+
+
+// edit a product 
+const editProduct = async (req, res) => {
+    const { id } = req.params;
+    const { image, title, description, category, brand, price, salePrice, totalStock } = req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(id, { image, title, description, category, brand, price, salePrice, totalStock }, { new: true });
+
+    if (updatedProduct === null) {
+        throw new ApiError(404, "Product doesn't exist")
+    }
+    res.status(200).json(new ApiResponse(200, updatedProduct, "Successfully Updated Product"))
+
+}
+
+// delete a product
+const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    if (deletedProduct === null) {
+        throw new ApiError(404, "Product doesn't exist");
+    }
+
+    res.status(200).json(new ApiResponse(200, deletedProduct, "Successfully Deleted Product"));
+}
+
+export {
+    handleProductImageUpload,
+    addProduct,
+    fetchAllProducts,
+    editProduct,
+    deleteProduct,
+}
--- server/routes/admin/products.routes.js
+++ server/routes/admin/products.routes.js
@@ -2,12 +2,15 @@
 import express from 'express';
 import { upload } from '../../helpers/multer.js';
 import { asyncHandler } from '../../utils/asyncHandler.js';
-import { handleProductImageUpload } from '../../controllers/admin/products.controller.js';
+import { handleProductImageUpload, addProduct, fetchAllProducts, editProduct, deleteProduct } from '../../controllers/admin/products.controller.js';
 
 const router = express.Router({ mergeParams: true });
 
 router.route("/image-upload").post(upload.single("my_file"), asyncHandler(handleProductImageUpload));
-
+router.route("/add").post(asyncHandler(addProduct));
+router.route("/get").get(asyncHandler(fetchAllProducts));
+router.route("/edit/:id").patch(asyncHandler(editProduct));
+router.route("/delete/:id").delete(asyncHandler(deleteProduct));
 
 
 export default router
