14c49f5120857c9c08336a50833eacc07bd11d49
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-11-28 13:25:51 +0530

    feat(client/shopping): add ShoppingProductTile component for product display in shopping listing

---
+++ client/src/components/shopping-view/ProductTile.jsx
@@ -0,0 +1,53 @@
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+
+function ShoppingProductTile({ product }) {
+  return (
+    <Card className="w-full max-w-sm mx-auto">
+      <div className="">
+        <div className="relative">
+          <img
+            src={product?.image}
+            alt={product?.title}
+            className="object-cover w-full rounded-t-lg h-72"
+          />
+          {product.salePrice > 0 ? (
+            <Badge className="absolute bg-red-500 left-2 top-2 hover:bg-red-600">
+              Sale
+            </Badge>
+          ) : null}
+        </div>
+        <CardContent className="p-4">
+          <h2 className="mb-2 text-xl font-bold">{product?.title}</h2>
+          <div className="flex items-center justify-between mb-2">
+            <span className="text-sm text-muted-foreground">
+              {product?.category}
+            </span>
+            <span className="text-sm text-muted-foreground">
+              {product?.brand}
+            </span>
+          </div>
+          <div className="flex items-center justify-between mb-2">
+            <span
+              className={`${
+                product?.salePrice > 0 ? "line-through " : ""
+              }text-lg font-semibold text-primary`}
+            >
+              {product?.price}
+            </span>
+            {product?.salePrice > 0 ? (
+              <span className="text-lg font-semibold text-primary">
+                {product?.salePrice}
+              </span>
+            ) : null}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Add to Cart</Button>
+        </CardFooter>
+      </div>
+    </Card>
+  );
+}
+
+export default ShoppingProductTile;
--- client/src/pages/shopping-view/Listing.jsx
+++ client/src/pages/shopping-view/Listing.jsx
@@ -1,9 +1,9 @@
 import ProductFilter from "@/components/shopping-view/Filter";
+import ShoppingProductTile from "@/components/shopping-view/ProductTile";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuLabel,
   DropdownMenuRadioGroup,
   DropdownMenuRadioItem,
   DropdownMenuTrigger,
@@ -15,17 +15,17 @@ function ShoppingListing() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4">
       <ProductFilter />
-      <div className="bg-background w-full rounded-lg shadow-sm">
-        <div className="p-4 border-b flex items-center justify-between">
+      <div className="w-full rounded-lg shadow-sm bg-background">
+        <div className="flex items-center justify-between p-4 border-b">
           <h2 className="text-lg font-extrabold"> All Products </h2>
           <div className="flex items-center gap-2">
-            <span className="text-muted-foreground font-semibold">
+            <span className="font-semibold text-muted-foreground">
               10 Products
             </span>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="sm">
-                  <ArrowUpDownIcon className="h-6 w-6" />
+                  <ArrowUpDownIcon className="w-6 h-6" />
                   Sort by
                 </Button>
               </DropdownMenuTrigger>
@@ -40,6 +40,9 @@ function ShoppingListing() {
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
+        </div>
+        <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3">
+          <ShoppingProductTile />
         </div>
       </div>
     </div>
