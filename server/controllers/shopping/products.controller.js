9feb6779581ca1140830b6b0447fe2339449e2f0
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-12-03 21:38:59 +0530

    feat(server/shopping): add product details retrieval and error handling in getProductDetails function

--- server/controllers/shopping/products.controller.js
+++ server/controllers/shopping/products.controller.js
@@ -1,5 +1,7 @@
 import { Product } from '../../models/product.model.js';
 import { ApiResponse } from '../../utils/ApiResponse.js';
+import { ApiError } from '../../utils/ApiError.js';
+
 
 
 const getFilteredProducts = async (req, res) => {
@@ -34,11 +36,29 @@ const getFilteredProducts = async (req, res) => {
         default: sort.price = 1;
             break;
     }
-    const products = await Product.find( filters ).sort(sort);
+    const products = await Product.find(filters).sort(sort);
 
     res.status(200).json(new ApiResponse(200, products, "Successfully fetched Products"))
 }
 
+
+const getProductDetails = async (req, res) => {
+    const { productID } = req.params;
+
+
+    if (!productID) {
+        throw new ApiError(400, "productID is Missing");
+    }
+    const product = await Product.findById(productID);
+
+    if (!product) {
+        throw new ApiError(404, "Product doesn't exist")
+    }
+
+    res.status(200)
+        .json(new ApiResponse(200, product, "successfully fetched product details"))
+}
 export {
-    getFilteredProducts
+    getFilteredProducts,
+    getProductDetails
 }
--- server/routes/shopping/products.routes.js
+++ server/routes/shopping/products.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { asyncHandler } from '../../utils/asyncHandler.js';
-import { getFilteredProducts } from '../../controllers/shopping/products.controller.js';
+import { getFilteredProducts, getProductDetails } from '../../controllers/shopping/products.controller.js';
 
 
 const router = Router({ mergeParams: true })
 
-router.route("/get").get(asyncHandler(getFilteredProducts))
+router.route("/get").get(asyncHandler(getFilteredProducts));
+router.route("/get/:productID").get(asyncHandler(getProductDetails));
 
 export default router;
