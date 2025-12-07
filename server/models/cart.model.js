b0681fa874377cd2bd7004830edc5e93cc892ff0
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-12-07 15:03:54 +0530

    fix(server/cart): correct typo in required field for userId in cart schema

--- server/models/cart.model.js
+++ server/models/cart.model.js
@@ -4,7 +4,7 @@ const cartSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: ture,
+        required: true,
         unique: true,
     },
     items: [
