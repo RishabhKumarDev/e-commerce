857f0b29cf847d08b95b6ed51320fc532efcb4ba
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-12-12 22:06:37 +0530

    feat(server/orders): implement order creation with PayPal integration and define order model

---
+++ server/controllers/shopping/order.controller.js
@@ -0,0 +1,80 @@
+import paypal from '../../helpers/paypal.js';
+import { Order } from '../../models/order.model.js';
+import { ApiError } from '../../utils/ApiError.js';
+import { ApiResponse } from '../../utils/ApiResponse.js';
+
+const createOrder = async (req, res) => {
+    const {
+        userId,
+        cartItems,
+        address,
+        orderStatus,
+        paymentMethod,
+        paymentStatus,
+        totalAmount,
+        orderDate,
+        orderUpdateDate,
+        paymentId,
+        payerId,
+    } = req.body;
+
+    const create_payment_json = {
+        intent: "sale",
+        payer: {
+            payment_method: "paypal"
+        },
+        redirect_urls: {
+            return_url: "http://localhost:5173/shopping/paypal-return",
+            calcle_url: "http://localhost:5173/shopping/paypal-cancel"
+        },
+        transactions: [
+            {
+                item_list: {
+                    items: cartItems.map(cartItem => ({
+                        name: cartItem.title,
+                        sku: cartItem.productId,
+                        price: cartItem.price.toFixed(2),
+                        currency: "USD",
+                        quantity: cartItem.quantity,
+                    }))
+                },
+                amount: {
+                    currency: "USD",
+                    total: totalAmount.toFixed(2)
+                },
+                description: "description"
+            }
+        ]
+    }
+
+    paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
+        if (error) {
+            throw new ApiError(500, "Error while creating payment")
+        } else {
+            const newOrder = new Order({
+                userId,
+                cartItems,
+                address,
+                orderStatus,
+                paymentMethod,
+                paymentStatus,
+                totalAmount,
+                orderDate,
+                orderUpdateDate,
+                paymentId,
+                payerId,
+            });
+
+            await newOrder.save();
+            const approvalURL = paymentInfo.links.find(link => link.rel === "approval_url").href;
+            res.status(201).json(new ApiResponse(201, {approvalURL, orderId: newOrder._id}))
+
+        }
+    })
+};
+const capturePayment = async (req, res) => { };
+
+export {
+    createOrder,
+    capturePayment
+}
---
+++ server/models/order.model.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema({
+    userId: String, 
+    cartItems:[
+        {
+            productId:String,
+            title:String,
+            image:String,
+            price:String,
+            quantity:Number,
+        }
+    ],
+    address:{
+        addressId:String,
+        address:String,
+        city:String,
+        pincode:String,
+        phone:String,
+        notes:String,
+    },
+    orderStatus:String,
+    paymentMethod:String,
+    paymentStatus:String,
+    totalAmount:String,
+    orderDate:Date,
+    orderUpdateDate:Date,
+    paymentId:String,
+    payerId:String,
+}, { timestamps: true });
+
+export const Order = mongoose.model("Order", orderSchema);
