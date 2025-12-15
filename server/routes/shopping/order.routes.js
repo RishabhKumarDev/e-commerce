5fdb44928f664f1759d25b59ee2de4c20b822679
Rishabh(aka-blue) <rishabhkumardev01@gmail.com>
2025-12-15 21:50:10 +0530

    feat(server/orders): add routes for order retrieval and details

--- server/routes/shopping/order.routes.js
+++ server/routes/shopping/order.routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { createOrder, capturePayment } from '../../controllers/shopping/order.controller.js';
+import { createOrder, capturePayment, getAllOrdersByUser, getOrderDetails } from '../../controllers/shopping/order.controller.js';
 import { asyncHandler } from '../../utils/asyncHandler.js';
 
 const router = Router({ mergeParams: true });
 
 router.route("/create").post(asyncHandler(createOrder));
+router.route("/capture").post(asyncHandler(capturePayment));
+router.route("/list/:userId").get(asyncHandler(getAllOrdersByUser));
+router.route("/details/:orderId").get(asyncHandler(getOrderDetails));
 
 
 export default router;
